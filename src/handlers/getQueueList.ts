7089dd5896b857a425ca574411b518d768c644f9
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-25 22:34:19 +0530

    new src v1.1.0

--- src/handlers/getQueueList.ts
+++ src/handlers/getQueueList.ts
@@ -1,15 +1,15 @@
-import { CustomSocket } from "../../types";
-import { getSongsWithVoteCounts } from "../lib/utils";
-import { errorHandler } from "./error";
+// import { CustomSocket } from "../../types";
+// import { getSongsWithVoteCounts } from "../lib/utils";
+// import { errorHandler } from "./error";
 
-export async function getQueueList(socket: CustomSocket) {
-  try {
-    const { roomInfo, userId } = socket;
-    if (!roomInfo || !userId) throw new Error("Login Required");
-    const queue = await getSongsWithVoteCounts(roomInfo._id, userId);
+// export async function getQueueList(socket: CustomSocket) {
+//   try {
+//     const { roomInfo, userId } = socket;
+//     if (!roomInfo || !userId) throw new Error("Login Required");
+//     const queue = await getSongsWithVoteCounts(roomInfo._id, userId);
 
-    socket.emit("queueList", queue);
-  } catch (error: any) {
-    errorHandler(socket, error.message);
-  }
-}
+//     socket.emit("queueList", queue);
+//   } catch (error: any) {
+//     errorHandler(socket, error.message);
+//   }
+// }
--- src/handlers/sendDuration.ts
+++ src/handlers/sendDuration.ts
@@ -1,13 +1,13 @@
-import { CustomSocket } from "../../types";
+// import { CustomSocket } from "../../types";
 
-import { errorHandler } from "./error";
+// import { errorHandler } from "./error";
 
-export async function sendProgress(socket: CustomSocket) {
-  try {
-    const { progress } = socket;
-    if (!progress) return;
-    socket.emit("updateProgress", progress);
-  } catch (error: any) {
-    errorHandler(socket, error.message);
-  }
-}
+// export async function sendProgress(socket: CustomSocket) {
+//   try {
+//     const { progress } = socket;
+//     if (!progress) return;
+//     socket.emit("updateProgress", progress);
+//   } catch (error: any) {
+//     errorHandler(socket, error.message);
+//   }
+// }
