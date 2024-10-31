0ff09e7a8a37f5c3c31769115a0a79ec64f7704f
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-01 01:46:24 +0530

    added rate limit

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,16 @@ import cookieParser from "cookie-parser";
 import useCors from "cors";
 import router from "./router/router";
 
+import { rateLimit } from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  limit: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
 const app = express();
 const server = createServer(app);
 
@@ -37,6 +47,7 @@ app.use(
   })
 );
 
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser()); // For cookie parsing
 app.use(router);
