d3f9979984926e3fcd2205a2557c354a2a848882
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-12 08:49:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -26,8 +26,8 @@ import { rateLimit } from "express-rate-limit";
 import { updateDetails } from "./handlers/updateDetails";
 
 const limiter = rateLimit({
-  windowMs: 2 * 60 * 1000, // 15 minutes
-  limit: 70, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 5 * 60 * 1000,
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   validate: {
