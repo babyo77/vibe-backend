95edc56f0e352cfd3587bf029bfcc9154c0ed270
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-16 22:18:41 +0530

    ✨ fixed profile api

--- src/functions/updateUserDP.ts
+++ src/functions/updateUserDP.ts
@@ -1,24 +1,28 @@
 import { Response } from "express";
 import { CustomRequest } from "../middleware/auth";
 import User from "../models/userModel";
+import { decryptObjectValues } from "../lib/utils";
 
 export const updateUserDp = async (req: CustomRequest, res: Response) => {
   try {
     const userId = req.userId;
-    const { imageUrl, imageDelUrl } = req.body;
+    const data = decryptObjectValues(req.body) as {
+      imageUrl: string;
+      imageDelUrl: string;
+    };
     if (!userId) throw new Error("Login required");
     if (
-      !imageUrl ||
-      !imageDelUrl ||
-      typeof imageUrl !== "string" ||
-      typeof imageDelUrl !== "string" ||
-      imageUrl.trim().length == 0 ||
-      imageDelUrl.trim().length == 0
+      !data.imageUrl ||
+      !data.imageDelUrl ||
+      typeof data.imageUrl !== "string" ||
+      typeof data.imageDelUrl !== "string" ||
+      data.imageUrl.trim().length == 0 ||
+      data.imageDelUrl.trim().length == 0
     )
       throw new Error("Image not provided 🙄");
     await User.findByIdAndUpdate(userId, {
-      imageUrl,
-      imageDelUrl,
+      imageUrl: data.imageUrl,
+      imageDelUrl: data.imageDelUrl,
     });
 
     res.send().status(204);
--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -1,9 +1,28 @@
 import { CustomSocket } from "../../types";
 import { encrypt } from "../lib/lock";
 
+function getRandomEmoji(emojis: string[]): string {
+  const randomIndex = Math.floor(Math.random() * emojis.length);
+  return emojis[randomIndex];
+}
+
 export async function errorHandler(socket: CustomSocket, message?: string) {
-  socket.emit(
-    "error",
-    encrypt(message + " " + "🤡" || "An unexpected error occurred")
-  );
+  const emojiArray = [
+    "😂",
+    "😎",
+    "😭",
+    "🥺",
+    "💀",
+    "🤡",
+    "🤪",
+    "🥴",
+    "😜",
+    "🫠",
+  ];
+
+  const randomEmoji = getRandomEmoji(emojiArray);
+
+  const finalMessage = message || "An unexpected error occurred";
+
+  socket.emit("error", encrypt(`${finalMessage} ${randomEmoji}`));
 }
