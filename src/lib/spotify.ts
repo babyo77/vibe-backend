86bab558c6cb2e016789e13dde0c262773720156
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-11-29 02:38:53 +0530

    Update spotify.ts

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,18 +1,12 @@
 import spotifyWebApi from "spotify-web-api-node";
-const secret = [
-  {
-    clientId: "ddf083a591a448089ae15e2ae725689c",
-    clientSecret: "d7299778cf514687a865fc9f280032fa",
-  },
-  {
-    clientId: "a9858d281030425a94171744d1921c7d",
-    clientSecret: "f3d3d6fb01ec49b59e0878f4c5515741",
-  },
-];
 
-const credentials = secret[Math.floor(Math.random() * secret.length)];
 const Spotify = new spotifyWebApi({
-  ...credentials,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
+(async () => {
+  const token = await Spotify.clientCredentialsGrant();
+  Spotify.setAccessToken(token.body["access_token"]);
+})();
 export default Spotify;
