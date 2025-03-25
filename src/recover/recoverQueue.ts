23808cced1deb098cf03ae100e7b97c53eb8b7d9
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-03-25 21:01:34 +0530

    Improve deleteDeletedQueue function to include empty update object in updateMany call

--- src/recover/recoverQueue.ts
+++ src/recover/recoverQueue.ts
@@ -3,10 +3,13 @@ import Queue from "../models/queueModel";
 async function deleteDeletedQueue() {
   try {
     // Delete all rooms marked as not deleted in one operation
-    const result = await Queue.updateMany({
-      roomId: "",
-      deleted: false,
-    });
+    const result = await Queue.updateMany(
+      {
+        roomId: "",
+        deleted: false,
+      },
+      {}
+    );
     console.log(`Deleted ${result.modifiedCount} rooms marked as not deleted.`);
 
     console.log("Completed not-deleted room cleanup process.");
